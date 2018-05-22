bfeb47cd9268215791066d05ed9e8cda0e8b3f20
anuutama@gmail.com <anuutama@gmail.com>
2018-05-22 23:34:07 +0800

    update

--- development/RELEASE v1.01/3_QKDComm/send_32bitQKD.py
+++ development/RELEASE v1.01/3_QKDComm/send_32bitQKD.py
@@ -11,27 +11,33 @@ import sys
 import time
 
 # Parameter
-rep_wait_time = 0.5  # 500 ms to wait for reply from the other side
+rep_wait_time = 0.3  # Wait time between packets (in s).
 
 ''' Helper functions '''
 
 def send4BytesC(message_str):
     if len(message_str) == 4:
-        print "HERE"
-        deviceC.write('SEND ') # Flag to send
-        deviceC.write(message_str)
+        deviceC.write('SEND ') # Send identifier [BEL]x3 + A
+        deviceC.write(b'\x07\x07\x07' + 'A') # This is Alice (sender)
         time.sleep(rep_wait_time)
+        deviceC.write('SEND ') # Send message
+        deviceC.write(message_str)
     else:
         print "The message is not 4 bytes. Please check again"
 
 def recv4BytesC():
     deviceC.write('RECV ') # Flag to recv
-    while True:            # Block until receives a reply
+    state = 0   # 0: waiting for STX, 1: transmitting/ wait for ETX
+    while True: # Block until receives a reply
         if deviceC.in_waiting:
-            hex_string = deviceC.read(8)
-            break
+            if hex_string == '7070742':  # 07-[BEL], 42-B (Header from Bob)
+                print ("Received message!") # Debug
+                state = 1
+            elif state == 1:
+                hex_string = deviceC.read(8)
+                break
     # Convert to ASCII string
-    hex_list= map(''.join, zip(*[iter(hex_string)]*2))
+    hex_list = map(''.join, zip(*[iter(hex_string)]*2))
     ascii_string = "".join([chr(int("0x"+each_hex,0)) for each_hex in hex_list])
     return ascii_string
 
@@ -58,6 +64,8 @@ print "Here we go"
 
 try:
     send4BytesC("HELL")
+    send4BytesC("BLAH")
+    print recv4BytesC()
     print recv4BytesC()
 except KeyboardInterrupt:
     # End of program
